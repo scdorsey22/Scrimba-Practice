49beab573cfa0f491b3b97fab58a82cc384e37f1
Shane Dorsey <scdorsey22@gmail.com>
2023-01-31 17:26:46 -0800

    scrimba static page

--- scrimba-practice/src/App.js
+++ scrimba-practice/src/App.js
@@ -1,11 +1,21 @@
+
+import './logo.svg'
 import './App.css';
 import NavBar from './NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-    </div>
+    <div>
+    <img src="./react-logo.png" width="40px" alt="react logo"/>
+    <h1>Fun facts about React</h1>
+    <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+</div>
   );
 }
 
--- scrimba-practice/src/NavBar.js
+++ scrimba-practice/src/NavBar.js
@@ -4,6 +4,9 @@ export default function NavBar() {
 
     return(
         <nav>
+            <div>
+                <img src="./react-logo.png" width="40px" alt="react" />
+            </div>
             <h1>Bob's Bistro</h1>
             <ul>
                 <li>Menu</li>
