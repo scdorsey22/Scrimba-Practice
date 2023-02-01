21dbcdfd89bd84e68fa99a19ea36ee1968b2b214
Shane Dorsey <scdorsey22@gmail.com>
2023-01-31 17:01:58 -0800

    scrimba react

---
+++ scrimba-practice/src/NavBar.js
@@ -0,0 +1,15 @@
+
+
+export default function NavBar() {
+
+    return(
+        <nav>
+            <h1>Bob's Bistro</h1>
+            <ul>
+                <li>Menu</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
